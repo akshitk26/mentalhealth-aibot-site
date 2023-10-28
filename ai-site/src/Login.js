c0a7423042b52ae4711ea2c26f9594a35815536e
darxx26 <akshit2k26@gmail.com>
2023-10-28 17:00:07 -0400

    login css

--- ai-site/src/Login.js
+++ ai-site/src/Login.js
@@ -5,7 +5,49 @@ import './Login.css';
 function Login() {
   return (
     <div className='Login'>
-      <h2>Login Page</h2>
+      <div className='main'>
+        <div className='loginBox'>
+
+            <div className='signUpBox'>
+                <div className='formBox'>
+                    <h1>Sign Up</h1>
+                    <form>
+                        <div className='inputBox'>
+                            <span class='icon'></span>
+                            <input type="email" required></input>
+                            <label>Email</label>                          
+                        </div>
+                        <div className='inputBox'>
+                            <span class='icon'></span>
+                            <input type="password" required></input>
+                            <label>Password</label>
+                        </div>
+                        <button type='submit' class='btn' id='signupButton'>Sign Up</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className='signInBox'>
+                < div className='formBox'>
+                    <h1>Login</h1>
+                    <form>
+                        <div className='inputBox'>
+                            <span class='icon'></span>
+                            <input type="email" required></input>
+                            <label>Email</label>                          
+                        </div>
+                        <div className='inputBox'>
+                            <span class='icon'></span>
+                            <input type="password" required></input>
+                            <label>Password</label>
+                        </div>
+                        <button type='submit' class='btn'id='loginButton'>Login</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+      </div>
     </div>
   );
 }
