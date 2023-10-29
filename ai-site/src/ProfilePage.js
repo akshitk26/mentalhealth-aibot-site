1d06f4ca3d6103b104526d49ff229b4747fce0a1
darxx26 <akshit2k26@gmail.com>
2023-10-28 23:33:41 -0400

    profile

--- ai-site/src/ProfilePage.js
+++ ai-site/src/ProfilePage.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ProfilePage.css';
+import { onAuthStateChanged, getAuth, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 function ProfilePage() {
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        // Check authentication status when the component mounts
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+          setUser(user);
+        });
+    
+        // Clean up the subscription when the component unmounts
+        return () => unsubscribe();
+      }, [auth]);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     return (
-        <div>hola boza</div>
+        <div className='ProfilePage'>
+            <div>
+                {user ? ( // Check if the user is authenticated
+                    <div>
+                        <h1>Welcome, {user.displayName || user.email}!</h1>
+                        <button onClick={handleSignOut}>Sign Out</button>
+                        {/* Additional content for logged-in users */}
+                    </div>
+                ) : (
+                    <div>
+                        <h1>Access Denied</h1>
+                        <p>Please sign in or sign up to view this page.</p>
+                        {/* Additional content for non-logged-in users */}
+                    </div>
+                )}
+            </div>
+        </div>
+        
     );
 }
 
