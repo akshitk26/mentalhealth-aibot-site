39abe0c816dfefd899cd6aeac8aed939ca8ec857
darxx26 <akshit2k26@gmail.com>
2023-10-28 23:43:57 -0400

    env vars

--- ai-site/src/firebase.js
+++ ai-site/src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwkyFWnz30TIMQ5u4sqhWvJO5Epe9oWkY",
-  authDomain: "mentalhealth-bot-site.firebaseapp.com",
-  projectId: "mentalhealth-bot-site",
-  storageBucket: "mentalhealth-bot-site.appspot.com",
-  messagingSenderId: "1049371335040",
-  appId: "1:1049371335040:web:38eefde53405d63f79c965"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
