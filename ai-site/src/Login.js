7fab25af769137e902814531eaf3533180ee4991
darxx26 <akshit2k26@gmail.com>
2023-10-29 18:15:52 -0400

    login.js changes

--- ai-site/src/Login.js
+++ ai-site/src/Login.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import './Login.css';
 import {auth} from './firebase'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged, getAuth, signOut } from 'firebase/auth';
 
 function Login() {
 
@@ -17,8 +18,11 @@ function Login() {
         e.preventDefault();
 
         try {
-            const user = await createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
+            const userCredential = await createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
             console.log('User created: ', user);
+            const newUser = userCredential.user;
+
+            setUser(newUser);
 
             //redierct to profile page after successful signup
             console.log('redirecting to profile page')
@@ -32,8 +36,11 @@ function Login() {
         e.preventDefault();
 
         try {
-            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
             console.log(user);
+            const loggedInUser = userCredential.user;
+
+            setUser(loggedInUser);
 
             //redierct to profile page after successful signup
             console.log('redirecting to profile page')
@@ -43,70 +50,92 @@ function Login() {
         }
      };
 
+     const [user, setUser] = useState(null);
+     const auth = getAuth();
+ 
+     useEffect(() => {
+         // Check authentication status when the component mounts
+         const unsubscribe = onAuthStateChanged(auth, (user) => {
+           setUser(user);
+         });
+     
+         // Clean up the subscription when the component unmounts
+         return () => unsubscribe();
+       }, [auth]);
+
   return (
     <div className='Login'>
       <div className='main'>
-        <div className='loginBox'>
 
-            <div className='signUpBox'>
-                <div className='formBox'>
-                    <h1>Sign Up</h1>
-                    <form onSubmit={register}>
-
-                        <div className='inputBox'>
-                            <span class='icon'></span>
-                            <input type="email" required
-                                onChange={(event) => {
-                                    setSignupEmail(event.target.value)
-                                    }}>        
-                            </input>
-                            <label>Email</label>                          
-                        </div>
-
-                        <div className='inputBox'>
-                            <span class='icon'></span>
-                            <input type="password" required 
-                                onChange={(event) => {
-                                    setSignupPassword(event.target.value)
-                                    }}>
-                            </input>
-                            <label>Password</label>
-                        </div>
-
-                        <button type='submit' class='btn' id='signupButton' onClick={register}>Sign Up</button>
-                    </form>
+      {user ? (
+                <div className='signedInMsg'>
+                    <p>You're already logged in! If you want to sign out, head to the profile page.</p>
                 </div>
-            </div>
+            ) : (
+                <div>
+                    <div className='loginBox'>
+                        <div className='signUpBox'>
+                            <div className='formBox'>
+                                <h1>Sign Up</h1>
+                                <form onSubmit={register}>
 
-            <div className='signInBox'>
-                < div className='formBox'>
-                    <h1>Login</h1>
-                    <form onSubmit={login}>
+                                    <div className='inputBox'>
+                                        <span class='icon'></span>
+                                        <input type="email" required
+                                            onChange={(event) => {
+                                                setSignupEmail(event.target.value)
+                                                }}>        
+                                        </input>
+                                        <label>Email</label>                          
+                                    </div>
 
-                        <div className='inputBox'>
-                            <span class='icon'></span>
-                            <input type="email" required
-                                onChange={(event) => {
-                                    setLoginEmail(event.target.value)
-                                    }}></input>
-                            <label>Email</label>                          
+                                    <div className='inputBox'>
+                                        <span class='icon'></span>
+                                        <input type="password" required 
+                                            onChange={(event) => {
+                                                setSignupPassword(event.target.value)
+                                                }}>
+                                        </input>
+                                        <label>Password</label>
+                                    </div>
+
+                                    <button type='submit' class='btn' id='signupButton' onClick={register}>Sign Up</button>
+                                </form>
+                            </div>
                         </div>
 
-                        <div className='inputBox'>
-                            <span class='icon'></span>
-                            <input type="password" required
-                            onChange={(event) => {
-                                setLoginPassword(event.target.value)
-                                }}></input>
-                            <label>Password</label>
+                        <div className='signInBox'>
+                            < div className='formBox'>
+                                <h1>Login</h1>
+                                <form onSubmit={login}>
+
+                                    <div className='inputBox'>
+                                        <span class='icon'></span>
+                                        <input type="email" required
+                                            onChange={(event) => {
+                                                setLoginEmail(event.target.value)
+                                                }}></input>
+                                        <label>Email</label>                          
+                                    </div>
+
+                                    <div className='inputBox'>
+                                        <span class='icon'></span>
+                                        <input type="password" required
+                                        onChange={(event) => {
+                                            setLoginPassword(event.target.value)
+                                            }}></input>
+                                        <label>Password</label>
+                                    </div>
+
+                                    <button type='submit' class='btn'id='loginButton' onClick={login}>Login</button>
+                                </form>
+                            </div>
                         </div>
-
-                        <button type='submit' class='btn'id='loginButton' onClick={login}>Login</button>
-                    </form>
-                </div>
-            </div>
-
         </div>
+                </div>
+            )}
+
+        
       </div>
     </div>
   );
